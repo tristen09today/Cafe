f49000ee4af736f2aeff4de59447fc50a605a3ae
Tristen Shieh <tristenshieh09@gmail.com>
2025-05-23 21:06:14 +1200

    Elikgibility and Constant

--- Version1.py
+++ Version1.py
@@ -1,2 +1,12 @@
-#This is version 1 of the cafe program
-#This program is a simple cafe program that takes orders and calculates the total cost
+#Version 1 of the app should be a simple console program with no GUI.
+#make sure to use constants for variables that are used in the program
+#year eligibility for the app is 9-13
+
+YEAR_ELIGIBILITY = [9, 10, 11, 12, 13]
+
+year_level=int(input("Enter your year level: "))
+if year_level in YEAR_ELIGIBILITY:
+    print("Welcome to the café app!")
+else:
+    print("You are not eligible to use the app.")
+    exit()
